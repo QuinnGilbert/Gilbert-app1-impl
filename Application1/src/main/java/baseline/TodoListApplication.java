ee5cb930c1bf0104db5ee3c4d5956724f50c5af4
QuinnG <quinngilbert12and3@gmail.com>
2021-11-05 22:21:02 -0400

    Added Initial

---
+++ Application1/src/main/java/baseline/TodoListApplication.java
@@ -0,0 +1,28 @@
+/*
+ *  UCF COP3330 Fall 2021 Application Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+package baseline;
+import javafx.application.Application;
+import javafx.scene.*;
+import javafx.fxml.*;
+import javafx.stage.Stage;
+
+
+public class TodoListApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("viewList.fxml"));
+
+        Scene scene = new Scene(root);
+        stage.setTitle("To-Do List Manager");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ Application1/src/main/java/baseline/viewListController.java
@@ -0,0 +1,21 @@
+package baseline;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+public class viewListController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    void addItem(ActionEvent event) {
+
+    }
+
+}
